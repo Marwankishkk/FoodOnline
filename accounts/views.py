9b8199f96ab2d289599906c565cd7925b3efe14c
Marwan Kishk <kishkmarwan49@gmail.com>
2026-03-02 01:35:30 +0200

    Implemnet Vendor Registiration

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserForm
 from .models import User
+from vendor.forms import VendorForm
+from accounts.models import UserProfile
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
@@ -23,3 +25,35 @@ def registerUser(request):
         'form': form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+def registerVendor(request):
+    if request.method == 'POST':
+        vendor_form = VendorForm(request.POST, request.FILES)
+        user_form = UserForm(request.POST)
+        if vendor_form.is_valid() and user_form.is_valid():
+            user_cleaned_data = user_form.cleaned_data
+            vendor_cleaned_data = vendor_form.cleaned_data
+            user = User.objects.create_user(
+                first_name=user_cleaned_data['first_name'],
+                last_name=user_cleaned_data['last_name'],
+                username=user_cleaned_data['username'],
+                email=user_cleaned_data['email'],
+                password=user_cleaned_data['password'],
+            )
+            user.role = User.VENDOR
+            user.save()
+            vendor = vendor_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Your account has been registered successfully!')
+            return redirect('registerVendor')
+    else:
+        vendor_form = VendorForm()
+        user_form = UserForm()
+    context = {
+        'user_form': user_form,
+        'vendor_form': vendor_form,
+    }
+    return render(request, 'accounts/registerVendor.html', context)
---
+++ vendor/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .forms import VendorForm
+from accounts.forms import UserForm
+from accounts.models import User
+# Create your views here.
